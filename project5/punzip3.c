4fd0a287d4968c6ee2eee761630f9bc9883e4869
jepuli124 <konsta.jalkanen.kj@gmail.com>
2025-03-21 11:11:12 +0200

    projcet 5

---
+++ project5/punzip3.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <sys/file.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <sys/sysinfo.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <math.h>
+
+// this has to packed so it is exactly 5 bytes
+typedef struct {
+    __int32_t n;
+    char c;
+} __attribute__((packed)) data_t;
+
+typedef struct {
+    char * fm;
+    size_t start_index; // inclusive
+
+} pthread_arg_t;
+
+void decompress_file(pthread_arg_t *p_args, char ** returnValue) {
+    data_t buffer;
+
+    char * text = malloc(sizeof(char)*2); //allocating start memory
+    text[0] = '\0';
+    // read data into buffer
+    buffer.n = (p_args->fm[p_args->start_index+3] << 24) | (p_args->fm[p_args->start_index+2] << 16) | (p_args->fm[p_args->start_index+1] << 8) | (p_args->fm[p_args->start_index]);
+    buffer.c = p_args->fm[p_args->start_index+4];
+
+    
+    // print the given character the given number of times
+    for (size_t i = 0; i < buffer.n; i++) {
+        text[strlen(text)+1] = '\0'; // adding end of text mark to prevent random symbols from new memory to spawn 
+        if ((text = realloc(text, sizeof(char)*(strlen(text)+5))) == NULL) { //allocating correct amount of memory
+            perror("realloc didn't succeed \n");
+            } 
+        text[strlen(text)] = buffer.c;
+    }
+    
+    //printf("inside decompress file:\n %s\n", text);
+    text[strlen(text)+1] = '\0'; //adding end mark
+    *returnValue = text;
+    text = NULL;
+    return;
+}
+
+void * runThread(void *p_args){
+
+    char * returnValues;
+    decompress_file((pthread_arg_t *) p_args, &returnValues);
+
+    return returnValues;
+}
+
+size_t get_file_length(char *file_name) {
+    FILE *file;
+    long len;
+    // try to open file
+    if ((file = fopen(file_name, "r"))){
+        fseek(file, 0L, SEEK_END);
+        len = ftell(file);
+    } else {
+        len = 0;
+    }
+    fclose(file);
+    return len;
+}
+
+int main(int argc, char *argv[]) {
+    // if atleast 1 file is not given, exit
+    if (argc < 2) {
+        printf("wunzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    FILE *fp;
+
+    for (size_t i = 1; i < argc; i++) {
+        if ((fp = fopen(argv[i], "r"))){
+            size_t file_length = get_file_length(argv[i]); 
+            //printf("%li", file_length);
+            
+            char * tempfm = mmap(NULL, file_length, PROT_READ, MAP_PRIVATE, fileno(fp), 0);
+            if (tempfm == MAP_FAILED) {
+                perror("mmap failed");
+                exit(1);
+            }
+            
+            // prevents missing characters at end of file (hopefully)
+            //file_length += 1; 
+
+            // a way to prevent too many threads
+            size_t threadLimit = get_nprocs();
+
+            // the loop for threads.
+            for (size_t counter = 0; counter < file_length; counter += threadLimit * 5)
+            {
+                // each thread handles single characater 
+                size_t num_of_threads = 0; 
+                if (counter + (threadLimit * 5) > file_length) {
+                    num_of_threads = (size_t) floor((file_length - counter)/5);
+                } else {
+                    num_of_threads = threadLimit;
+                }
+                void ** returnValues = malloc(sizeof(char *) * (num_of_threads));
+                
+                
+                pthread_arg_t * p_args = malloc(sizeof(pthread_arg_t) * (num_of_threads));
+
+                // set args
+                
+                p_args[0].fm = tempfm;
+                p_args[0].start_index = counter;
+
+
+                for (size_t i = 1; i < num_of_threads; i++) {
+                    p_args[i].fm = tempfm;
+                    p_args[i].start_index = counter + (i * 5);
+
+                }
+                
+                pthread_t * thread_id = malloc(sizeof(pthread_t) * (num_of_threads));    // Allocating a slot for each thread. https://www.geeksforgeeks.org/multithreading-in-c/
+
+                for (size_t i = 0; i < num_of_threads; i++) { //creating a thread for each file.
+                    pthread_create(&thread_id[i], NULL, runThread, &p_args[i]);
+                    
+                }
+
+                for (size_t i = 0; i < num_of_threads; i++) { //waiting that all threads complete
+                    pthread_join(thread_id[i], &returnValues[i]);
+                }
+                
+                for (size_t i = 0; i < num_of_threads; i++) { //printing out values
+                    printf("%s", (char *) returnValues[i]);
+                    
+                }
+                for (size_t i = 0; i < num_of_threads; i++) { // free memory
+                    // free(p_args[i].fm);
+                    // p_args[i].fm = NULL;
+                    
+                    free(returnValues[i]);
+                }
+                free(thread_id);
+                free(p_args);
+                free(returnValues);
+                p_args = NULL;
+                returnValues = NULL;
+                
+            }
+
+            fclose(fp);
+            fp = NULL;
+            tempfm = NULL;
+            
+        } else {
+            // if opening fails, exit
+            printf("failed opening file %s\n", argv[i]);
+            exit(1);
+        }
+    }
+    exit(0);
+}
---
+++ project5/pzipV4.c
@@ -0,0 +1,217 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/sysinfo.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+    __int32_t n;
+    char c;
+} __attribute__((packed)) data_t;
+
+typedef struct {
+    char * fm;
+    size_t start_index; // inclusive
+    size_t end_index; // exclusive
+} pthread_arg_t;
+
+typedef struct {
+    data_t * data;
+    size_t last_used_index;
+} pthread_ret_t;
+
+size_t get_file_length(char *file_name) {
+    FILE *file;
+    long len;
+    // try to open file
+    if ((file = fopen(file_name, "r"))){
+        fseek(file, 0L, SEEK_END);
+        len = ftell(file);
+    } else {
+        len = 0;
+    }
+    fclose(file);
+    return len;
+}
+
+void *p_compress(void *pthread_args) {
+    pthread_arg_t *args = (pthread_arg_t*) pthread_args;
+
+    pthread_ret_t *ret = malloc(sizeof *ret);
+    ret->data = malloc(sizeof(data_t) * (args->end_index - args->start_index));
+
+    // begin zipping
+    char prev_char = args->fm[args->start_index];
+    __int32_t length = 0;
+    size_t file_index = args->start_index;
+    size_t data_index = 0;
+
+    while (file_index < args->end_index) {
+        // check if the character if the same as previous
+        if ((args->fm[file_index] == prev_char)) {
+            // add 1 to length if it is
+            length++;
+        } else {
+            // write to data if it is not
+            ret->data[data_index].n = length;
+            ret->data[data_index].c = prev_char;
+            data_index++;
+            length = 1;
+        }
+        // set previous character
+        prev_char = args->fm[file_index];
+        file_index++;
+    }
+
+    ret->data[data_index].n = length;
+    ret->data[data_index].c = prev_char;
+    ret->last_used_index = data_index;
+
+    pthread_exit(ret);
+}
+
+void *write_chunk(void *args) {
+    pthread_ret_t *pret = (pthread_ret_t *) args;
+    fwrite(pret->data, sizeof(data_t), pret->last_used_index + 1, stdout);
+    
+    pthread_exit(NULL);
+}
+
+
+void compress_file(char *fm, size_t file_length, size_t num_of_threads /*, char lastLetter */) {
+
+    // init arrays for passing to and retrieving from threads
+    pthread_t pThreads [num_of_threads];
+    pthread_arg_t p_args [num_of_threads];
+    pthread_ret_t *p_rets [num_of_threads];
+    
+    // divide file evenly for threads
+    size_t thread_len [num_of_threads];
+
+    for (size_t i = 0; i < num_of_threads; i++) thread_len[i] = file_length / num_of_threads;
+    for (size_t i = 1; i < file_length % num_of_threads; i++) thread_len[(num_of_threads - i) % num_of_threads]++;
+
+    // move a part of the length from first thread to last
+    size_t change = thread_len[0] * 8 / 10;
+    thread_len[0] -= change;
+    thread_len[num_of_threads - 1] += change;
+
+    // set args
+    p_args[0].fm = fm;
+    p_args[0].start_index = 0;
+    p_args[0].end_index = thread_len[0];
+
+    for (size_t i = 1; i < num_of_threads; i++) {
+        p_args[i].fm = fm;
+        p_args[i].start_index = p_args[i - 1].end_index;
+        p_args[i].end_index = p_args[i].start_index + thread_len[i];
+    }
+
+    // create threads
+    for (size_t i = 0; i < num_of_threads; i++) {
+        pthread_create(&pThreads[i], NULL, p_compress, &p_args[i]);
+    }
+
+    // join the first thread and get data
+    // other threads will be joined in the loop
+    pthread_join(pThreads[0], (void**) &p_rets[0]);
+
+    
+
+    pthread_t write_pthread;
+
+    // merge threads and write data
+    for (size_t thread = 0; thread < num_of_threads; thread++) {
+        //printf("%d %c\n", p_rets[thread]->data->n, p_rets[thread]->data->c);
+
+        if (thread + 1 < num_of_threads ) {
+            // if this is the last chunk of data from this thread
+            // make sure the next thread has finished
+            pthread_join(pThreads[thread + 1], (void**) &p_rets[thread + 1]);
+            
+
+            if (p_rets[thread]->data[p_rets[thread]->last_used_index].c == p_rets[thread + 1]->data[0].c) {
+                // if next chunk would be the same char, add the repetitions to the next one
+
+                p_rets[thread + 1]->data[0].n += p_rets[thread]->data[p_rets[thread]->last_used_index].n;
+                p_rets[thread]->data[p_rets[thread]->last_used_index].n = 1;
+                p_rets[thread]->last_used_index -= 1;
+            }
+
+            // if(thread == 0 && p_rets[thread + 1]->data[0].c == lastLetter){
+
+            // }
+
+        }
+
+        // write the data
+        if (thread > 0) {
+            pthread_join(write_pthread, (void**) NULL);
+        }
+        pthread_create(&write_pthread, NULL, write_chunk, p_rets[thread]);
+    }
+    pthread_join(write_pthread, (void**) NULL);
+    
+    // free memory
+    for (size_t i = 0; i < num_of_threads; i++) {
+        free(p_rets[i]->data);
+        p_rets[i]->data = NULL;
+        free(p_rets[i]);
+        p_rets[i] = NULL;
+    }
+
+} 
+
+//idk what is wrong, this returns the last character of the file bubblegumfix
+// char lastCharOfFile(){
+    
+//     return
+// }
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf("wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    FILE *fp;
+    char * fm = NULL;
+    size_t currentSize = 3;
+    fm = malloc(sizeof(char) * currentSize);
+    size_t file_length = 0;
+
+    // This is not a smart solution but atleast on my machine this is close to the optimal number of threads.
+    // From my testing I can say that writing data is the slowest part of this program.
+    // Increasing the number of threads makes it so you can start writing earlier 
+    // since each data chunk is smaller and thus takes less time to compute.
+    // But too many threads will slow down the program a lot (maybe the writing thread gets pushed back??)
+    // I am too lazy to re-write this program to have a permanent writing thread 
+    // that would get paused when no data is available for writing
+    size_t num_of_threads = get_nprocs();
+
+    for (size_t i = 1; i < argc; i++) {
+        if ((fp = fopen(argv[i], "r"))){
+            file_length += get_file_length(argv[i]); 
+            currentSize += file_length;
+            char * tempfm = mmap(NULL, file_length, PROT_READ, MAP_PRIVATE, fileno(fp), 0);
+            fm = realloc(fm, sizeof(char)*currentSize);
+            strcat(fm, tempfm);
+            fclose(fp);
+            fp = NULL;
+            tempfm = NULL;
+            
+        } else {
+            // if opening fails, exit
+            printf("failed opening file %s\n", argv[i]);
+            exit(1);
+        }
+    }
+    file_length += 1;             // fixes files that doesn't have empty line at end of the file 
+    compress_file(fm, file_length, num_of_threads);
+    free(fm);
+    fm = NULL;
+    exit(0);
+}
